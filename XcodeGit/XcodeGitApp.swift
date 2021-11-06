47fa589652b277c7ea789728222c89771d6410ad
ehayes <eric.hayes68@gmail.com>
2021-11-06 18:14:15 -0400

    Initial Commit

---
+++ XcodeGit/XcodeGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeGitApp.swift
+//  XcodeGit
+//
+//  Created by eric hayes on 11/6/21.
+//
+
+import SwiftUI
+
+@main
+struct XcodeGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
